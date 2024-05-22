c50ad9bcb11d09521a3796fe13e10bd311869a05
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-05-22 17:56:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from pyrogram.enums import ChatMemberStatus, ParseMode
 from pyrogram.enums import ChatType
 import asyncio
 import os
+import requests
 from os import getenv
 import traceback
 from pyrogram.types import Message
@@ -184,6 +185,41 @@ async def ban_all(client, msg):
                 pass
     else:
         await msg.reply_text("ɪ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴛʜᴇ ʀɪɢʜᴛ ᴛᴏ ʀᴇsᴛʀɪᴄᴛ ᴜsᴇʀs.")
+
+
+emojis = ["👍", "👎", "❤️", "🔥", "🥰", "👏", "😁", "🤔", "🤯", "😱", "🤬", "😢", "🎉", "🤩", "🤮", "💩", "🙏", "👌", "🕊", "🤡", "🥱", "🥴", "😍", "🐳", "❤️‍🔥", "🌚", "🌭", "💯", "🤣", "⚡️", "🍌", "🏆", "💔", "🤨", "😐", "🍓", "🍾", "💋", "🖕", "😈", "😴", "😭", "🤓", "👻", "👨‍💻", "👀", "🎃", "🙈", "😇", "😨", "🤝", "✍️", "🤗", "🫡", "🎅", "🎄", "☃️", "💅", "🤪", "🗿", "🆒", "💘", "🙉", "🦄", "😘", "💊", "🙊", "😎", "👾", "🤷‍♂️", "🤷", "🤷‍♀️", "😡"]
+
+app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=token)
+
+@app.on_message()
+async def react_to_message(client, message):
+    chat_id = message.chat.id
+    message_id = message.id
+    
+    # Choose a random emoji from the list
+    random_emoji = random.choice(emojis)
+    
+    url = f'https://api.telegram.org/bot{token}/setMessageReaction'
+
+    # Parameters for the request
+    params = {
+        'chat_id': chat_id,
+        'message_id': message_id,
+        'reaction': [{
+            "type": "emoji",
+            "emoji": random_emoji
+        }]
+    }
+
+    response = requests.post(url, json=params)
+
+    if response.status_code == 200:
+        print("Reaction set successfully!")
+        print("Response content:", response.content)
+    else:
+        print(f"Failed to set reaction. Status code: {response.status_code}")
+        print("Response content:", response.content)
+    
 
 
 app.run()
