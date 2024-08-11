8f782f2cc1449b228779198e00bcab071dd0ce94
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-08-11 19:53:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ import requests
 api_id = 26480985 #--Add your Api Id here
 api_hash = '56c935fae1c5c86ba5a3af655f8caa9d' #--Enter Api Hash Here
 
-token = '7326782027:AAEmMCnBlKFiHg1tZb3MkEu8MZ2YE_771cw' #--Enter Bot Token Here.
+token = '7326782027:AAFHcULYDfQ9-J4d4AJSx9zM98kxZyGTR0M' #--Enter Bot Token Here.
 
 app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=token)
 AM_PIC = [
